676fdde1048e9ec9b7f9dff63b98c9b089aaa9dd
jhj911135 <2713jhj@naver.com>
2021-07-02 11:27:57 +0900

    create class Variance

--- featuretools/primitives/premium/test.py
+++ featuretools/primitives/premium/test.py
@@ -5,12 +5,17 @@ from featuretools.entityset import Entity, EntitySet
 
 from pandas import DataFrame
 from featuretools.primitives import Correlation
-
-reference_data = pd.to_datetime("01-01-2019")
-input_ages = [pd.to_datetime("01-01-1989"), pd.to_datetime("01-01-2002")]
+from featuretools.primitives import Variance
 
 corr = Correlation(method='kendall')
 array_1 = [1, 4, 6, 7]
 array_2 = [1, 5, 9, 7]
 print(corr(array_1, array_2))
+
+variance = Variance()
+print(variance([0, 3, 4, 3]))
+
+
+
 exit(1)
+
